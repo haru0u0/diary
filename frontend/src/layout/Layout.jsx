cf9a755180935e7df053a96e8f5c1408849147e5
haruka <haruka.senzaki@gmail.com>
2024-08-05 21:59:20 +0100

    add:#7:create frontend of new entry page

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
 
 function Layout() {
   return (
     <>
-      <div className="flex justify-center m-5">
+      <div className="m-5">
         <Outlet />
       </div>
-      <Navbar />
     </>
   );
 }
---
+++ frontend/src/pages/New.jsx
@@ -0,0 +1,88 @@
+import { Link } from "react-router-dom";
+
+function New() {
+  function openModal() {
+    document.getElementById("dialog-rounded").showModal();
+  }
+
+  function closeModal() {
+    document.getElementById("dialog-rounded").close();
+  }
+
+  return (
+    <>
+      <div className="grid grid-cols-2 mb-2">
+        <Link
+          to="/calendar"
+          className="font-vt text-2xl justify-self-start self-center"
+        >
+          &times;
+        </Link>
+        <button
+          type="submit"
+          form="diary"
+          className="nes-btn is-primary justify-self-end text-lg pt-0 pb-0 self-center"
+        >
+          Submit
+        </button>
+      </div>
+      <form id="diary" method="post" className="block">
+        <textarea
+          name="Text1"
+          placeholder="How was your day?"
+          rows="5"
+          className="w-full border-0"
+          autoFocus
+        ></textarea>
+      </form>
+
+      <section>
+        <div className="fixed bottom-0 right-0 m-2">
+          <button onClick={() => openModal()}>
+            <img
+              src="../public/images/book.png"
+              className="object-contain h-12 w-12"
+            />
+          </button>
+        </div>
+        <dialog
+          className="nes-dialog is-rounded w-full h-full"
+          id="dialog-rounded"
+        >
+          <button className="text-2xl" onClick={() => closeModal()}>
+            &times;
+          </button>
+          <h2 className="text-2xl">Rounded dialog</h2>
+          <p>This is a dialog.</p>
+        </dialog>
+      </section>
+    </>
+  );
+}
+
+export default New;
+
+/*
+  function openModal() {
+    document.querySelector("#modal").classList.remove("hidden");
+  }
+
+  function closeModal() {
+    document.querySelector("#modal").classList.add("hidden");
+  }
+  
+        <div className="fixed bottom-0 right-0 m-2">
+        <button onClick={() => openModal()}>
+          <img
+            src="../public/images/book.png"
+            className="object-contain h-12 w-12"
+          />
+        </button>
+      </div>
+      <div className="nes-container with-title is-rounded hidden" id="modal">
+        <button className="text-2xl block" onClick={() => closeModal()}>
+          &times;
+        </button>
+        <p>Alert: this is a dialog.</p>
+      </div>
+  */
